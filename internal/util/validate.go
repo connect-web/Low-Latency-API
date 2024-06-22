5b0c91f48270f89d22e2c6cf582e2b13fa8bce03
Elliot <141949359+webElliot@users.noreply.github.com>
2024-06-22 14:05:34 +0100

    skill validation for params

---
+++ internal/util/validate.go
@@ -0,0 +1,60 @@
+package util
+
+import (
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
+	"strings"
+)
+
+var (
+	skillsMap = map[string]struct{}{
+		"attack":       {},
+		"defence":      {},
+		"strength":     {},
+		"hitpoints":    {},
+		"ranged":       {},
+		"prayer":       {},
+		"magic":        {},
+		"cooking":      {},
+		"woodcutting":  {},
+		"fletching":    {},
+		"fishing":      {},
+		"firemaking":   {},
+		"crafting":     {},
+		"smithing":     {},
+		"mining":       {},
+		"herblore":     {},
+		"agility":      {},
+		"thieving":     {},
+		"slayer":       {},
+		"farming":      {},
+		"runecraft":    {},
+		"hunter":       {},
+		"construction": {},
+	}
+)
+
+var Title = cases.Title(language.English)
+
+func ValidateSkills(skills []string) (validSkills []string) {
+	validSkills = make([]string, 0)
+	repeatPrevention := map[string]struct{}{}
+
+	for _, skill := range skills {
+		skill_lower := strings.ToLower(skill)
+
+		// prevent duplicate entries
+		_, isRepeated := repeatPrevention[skill_lower]
+		if isRepeated {
+			continue
+		}
+
+		// validate skill from pre-defined map
+		_, valid := skillsMap[skill_lower]
+		if valid {
+			validSkills = append(validSkills, Title.String(skill_lower))
+			repeatPrevention[skill_lower] = struct{}{}
+		}
+	}
+	return validSkills
+}
