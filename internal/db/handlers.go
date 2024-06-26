bf0c92497d0f1993051c6183536f2cca09f0ef95
Elliot <141949359+webElliot@users.noreply.github.com>
2024-06-26 12:33:11 +0100

    Added handles for SimplePlayer with different structs & Player model

--- internal/db/handlers.go
+++ internal/db/handlers.go
@@ -6,8 +6,7 @@ import (
 	"github.com/connect-web/Low-Latency-API/internal/model"
 )
 
-// HandleSimplePlayerRow processes a SQL row into a SimplePlayer struct.
-func HandleSimplePlayerRow(rows *sql.Rows) (model.SimplePlayer, error) {
+func HandleSimplePlayerRowRatio(rows *sql.Rows) (model.SimplePlayer, error) {
 	var plr model.SimplePlayer
 	var minigameBytes, skillRatioBytes []byte
 
@@ -19,6 +18,63 @@ func HandleSimplePlayerRow(rows *sql.Rows) (model.SimplePlayer, error) {
 		return plr, err
 	}
 	if err := json.Unmarshal(skillRatioBytes, &plr.SkillRatios); err != nil {
+		return plr, err
+	}
+
+	return plr, nil
+}
+
+func HandleSimplePlayerRowExperience(rows *sql.Rows) (model.SimplePlayer, error) {
+	var plr model.SimplePlayer
+	var minigameBytes, skillBytes []byte
+
+	if err := rows.Scan(&plr.Username, &skillBytes, &minigameBytes); err != nil {
+		return plr, err
+	}
+
+	if err := json.Unmarshal(minigameBytes, &plr.Minigames); err != nil {
+		return plr, err
+	}
+	if err := json.Unmarshal(skillBytes, &plr.Skills); err != nil {
+		return plr, err
+	}
+
+	return plr, nil
+}
+
+func HandleSimplePlayerRowLevel(rows *sql.Rows) (model.SimplePlayer, error) {
+	var plr model.SimplePlayer
+	var minigameBytes, skillLevelBytes []byte
+
+	if err := rows.Scan(&plr.Username, &skillLevelBytes, &minigameBytes); err != nil {
+		return plr, err
+	}
+
+	if err := json.Unmarshal(minigameBytes, &plr.Minigames); err != nil {
+		return plr, err
+	}
+	if err := json.Unmarshal(skillLevelBytes, &plr.SkillLevels); err != nil {
+		return plr, err
+	}
+
+	return plr, nil
+}
+
+func HandlePlayerRow(rows *sql.Rows) (model.Player, error) {
+	var plr model.Player
+	var minigameBytes, skillLevelBytes, skillExperienceGainBytes []byte
+
+	if err := rows.Scan(&plr.Username, &skillLevelBytes, &minigameBytes, &skillExperienceGainBytes); err != nil {
+		return plr, err
+	}
+
+	if err := json.Unmarshal(minigameBytes, &plr.Minigames); err != nil {
+		return plr, err
+	}
+	if err := json.Unmarshal(skillLevelBytes, &plr.SkillLevels); err != nil {
+		return plr, err
+	}
+	if err := json.Unmarshal(skillExperienceGainBytes, &plr.SkillGains); err != nil {
 		return plr, err
 	}
 
