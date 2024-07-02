b133ba0135aa56113b17d21a0d729a03e6f0031a
Elliot <141949359+webElliot@users.noreply.github.com>
2024-07-02 08:12:48 +0100

    added rate limits for API's
    - Added new API's for Minigame Hiscores (the minigame bots)

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gofiber/fiber/v3/middleware/limiter"
 	"log"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -20,9 +21,9 @@ func main() {
 
 	app.Use(limiter.New(limiter.Config{
 		Next: func(c fiber.Ctx) bool {
-			return c.IP() == "127.0.0.1"
+			return c.IP() == "127.0.0.1" || !strings.Contains(c.Path(), "api")
 		},
-		Max:        20,
+		Max:        5,
 		Expiration: 30 * time.Second,
 		KeyGenerator: func(c fiber.Ctx) string {
 			return c.Get("x-forwarded-for")
@@ -40,21 +41,15 @@ func main() {
 
 	//app.Get("/api/users", api.GetSimplePlayerFromName)
 
-	app.Get("/api/find-bots", api.GetPlayerFromSkills)
+	app.Get("/api/find-skill-bots", api.GetPlayerFromSkills)
+	app.Get("/api/find-minigame-bots", api.GetPlayerFromMinigames)
+	app.Get("/api/minigame-bots-hiscore", api.PlayerMinigameHiscores)
+	app.Get("/api/minigame-bots-listing", api.PlayerMinigameListing)
 
 	app.Static("/", "../../site/", fiber.Static{
-		CacheDuration: 30 * time.Minute,
-		Compress:      true,
-		Index:         "home.html",
+		// CacheDuration: 30 * time.Minute,
+		// Compress: true,
+		Index: "home.html",
 	})
-
-	envVar := os.Getenv("siteonline")
-	certDirectory := os.Getenv("certDir")
-
-	if envVar == "site" {
-		log.Fatal(app.Listen(":443", fiber.ListenConfig{CertFile: certDirectory + "fullchain.pem", CertKeyFile: certDirectory + "privkey.pem"}))
-	} else {
-		log.Fatal(app.Listen(":4050"))
-	}
 
 }
