88c609877bbca67bd03ec27898eba36d5eda9e94
Elliot <141949359+webElliot@users.noreply.github.com>
2024-07-02 08:12:58 +0100

    added method for site dev locally and online

--- cmd/webserver/config.go
+++ cmd/webserver/config.go
@@ -1,1 +1,11 @@
 package main
+
+func localAndServerDevelopment() {
+	envVar := os.Getenv("siteonline")
+	certDirectory := os.Getenv("certDir")
+	if envVar == "site" {
+		log.Fatal(app.Listen(":443", fiber.ListenConfig{CertFile: certDirectory + "fullchain.pem", CertKeyFile: certDirectory + "privkey.pem"}))
+	} else {
+		log.Fatal(app.Listen(":4050"))
+	}
+}
