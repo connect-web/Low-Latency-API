2e3fd10e418c15d7879cc1dfa2692a65473c32ac
Elliot <141949359+webElliot@users.noreply.github.com>
2024-07-04 00:12:21 +0100

    createSession implemented into Login and Register

--- internal/auth/handlers.go
+++ internal/auth/handlers.go
@@ -56,7 +56,16 @@ func Register(c fiber.Ctx) error {
 		return util.InternalServerError(c)
 	}
 
-	return c.JSON(fiber.Map{"message": "User registered"})
+	sessionErr := createSession(user.Username, c)
+	if sessionErr != nil {
+		// if the session fails to create on register then redirect to login
+		// frontend must redirect to login
+		return c.JSON(fiber.Map{"message": "User registered, Login required."})
+	} else {
+		// redirect to profile page
+		return c.JSON(fiber.Map{"message": "User registered"})
+	}
+
 }
 
 func Login(c fiber.Ctx) error {
@@ -85,37 +94,20 @@ func Login(c fiber.Ctx) error {
 		return util.InvalidCredentials(c)
 	}
 
-	// Use user session store for authenticated users
-	/*
-		sess, err := userSessionStore.Get(c)
-		if err != nil {
-			return err
-		}
-		sess.Set("username", user.Username)
-		if err := sess.Save(); err != nil {
-			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Session save error"})
-		}
+	sessionErr := createSession(user.Username, c)
+	if sessionErr == nil {
+		return c.JSON(fiber.Map{"message": "Logged in"})
+	}
 
-	*/
+	// this should not happen, maybe memory full?
 
-	return c.JSON(fiber.Map{"message": "Logged in"})
+	return util.InternalServerError(c)
+
 }
 
 func Logout(c fiber.Ctx) error {
 	fmt.Println("User sent logout request.")
-	sess, _ := userSessionStore.Get(c)
+	sess, _ := UserSessionStore.Get(c)
 	sess.Destroy()
 	return c.JSON(fiber.Map{"message": "Logged out"})
-}
-
-func Protected(c fiber.Ctx) error {
-	fmt.Println("User accessed protected route.")
-	sess, err := userSessionStore.Get(c)
-	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Session retrieval error"})
-	}
-	if sess.Get("username") == nil {
-		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "Unauthorized"})
-	}
-	return c.Next()
 }
