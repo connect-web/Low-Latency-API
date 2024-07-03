a2f57c4714558169de47f84792f23ee93ae95367
Elliot <141949359+webElliot@users.noreply.github.com>
2024-07-04 00:11:55 +0100

    added helmet middleware

--- internal/middleware/run.go
+++ internal/middleware/run.go
@@ -3,6 +3,7 @@ package middleware
 import (
 	"github.com/gofiber/fiber/v3"
 	"github.com/gofiber/fiber/v3/middleware/compress"
+	"github.com/gofiber/fiber/v3/middleware/helmet"
 	"github.com/gofiber/fiber/v3/middleware/logger"
 )
 
@@ -10,6 +11,8 @@ func Run(app *fiber.App) {
 	app.Use(compress.New(compress.Config{
 		Level: compress.LevelBestSpeed, // or compress.LevelBestCompression
 	}))
+
+	app.Use(helmet.New())
 
 	// Logging middleware
 	app.Use(logger.New(logger.Config{}))
