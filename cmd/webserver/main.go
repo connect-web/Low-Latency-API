f2988f5f35666e9b0bf8105ef0f3ab24a506054e
Elliot <141949359+webElliot@users.noreply.github.com>
2024-07-04 00:11:25 +0100

    simplification

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
-	"github.com/connect-web/Low-Latency-API/internal/api"
 	"github.com/connect-web/Low-Latency-API/internal/auth"
 	"github.com/connect-web/Low-Latency-API/internal/middleware"
+	"github.com/connect-web/Low-Latency-API/internal/protectedApis"
 	"github.com/gofiber/fiber/v3"
 	"log"
 	"os"
@@ -22,42 +22,10 @@ func main() {
 	// Initialize a new Fiber app
 	app := fiber.New()
 	// Use middlewares for each route
-	// app.Use(helmet.New(), )
 
-	middleware.Run(app)
+	middleware.Run(app) // Setup middleware
 
-	API(app)
-	StaticFiles(app)
-
-	auth.Main(app)
-	//auth.UnauthenticatedSessionSetup(app)
-
-	run(app)
-
-}
-
-func run(app *fiber.App) {
-	if front_end {
-		log.Fatal(app.Listen(":443", fiber.ListenConfig{CertFile: certDirectory + "fullchain.pem", CertKeyFile: certDirectory + "privkey.pem"}))
-	} else {
-		log.Fatal(app.Listen(":4050"))
-	}
-}
-
-func API(app *fiber.App) {
-	app.Get("/api/find-skill-bots", api.GetPlayerFromSkills)
-	app.Get("/api/find-minigame-bots", api.GetPlayerFromMinigames)
-	app.Get("/api/minigame-bots-hiscore", api.PlayerMinigameHiscores)
-	app.Get("/api/minigame-bots-listing", api.PlayerMinigameListing)
-
-	// Define a route for the GET method on the root path '/'
-	//app.Get("/api/ratio", api.GetPlayersByRatioHandler)
-	//app.Get("/api/experience", api.GetPlayersByExperienceHandler)
-	//app.Get("/api/levels", api.GetPlayersByLevelHandler)
-	//app.Get("/api/users", api.GetSimplePlayerFromName)
-}
-
-func StaticFiles(app *fiber.App) {
+	// Setup Static files routes
 	staticType := fiber.Static{
 		Index: "home",
 	}
@@ -66,4 +34,15 @@ func StaticFiles(app *fiber.App) {
 		staticType.Compress = true
 	}
 	app.Static("/", "../../site/")
+
+	auth.Setup(app) // Setup Register, Login Routes
+
+	protectedApis.Setup(app)
+
+	if front_end {
+		log.Fatal(app.Listen(":443", fiber.ListenConfig{CertFile: certDirectory + "fullchain.pem", CertKeyFile: certDirectory + "privkey.pem"}))
+	} else {
+		log.Fatal(app.Listen(":4050"))
+	}
+
 }
