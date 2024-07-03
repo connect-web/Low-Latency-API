e87912b7cbd4b47d11c6bcffdcf39abd227ad5e1
Elliot <141949359+webElliot@users.noreply.github.com>
2024-07-03 23:07:22 +0100

    added login css & csrf grabber

---
+++ site/resources/js/unauth-session.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    if (!document.cookie.split('; ').find(row => row.startsWith('csrf_='))) {
+        fetch('/api/csrf', {
+            method: 'GET',
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+            })
+            .catch(error => console.error('Error:', error));
+    }
+
+});
