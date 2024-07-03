92f67f53493222d16df0669a66ccffc769aa770f
Elliot <141949359+webElliot@users.noreply.github.com>
2024-07-04 00:12:56 +0100

    middleware now declared as global variable

--- internal/auth/api.go
+++ internal/auth/api.go
@@ -8,22 +8,24 @@ import (
 	"time"
 )
 
-var userSessionStore = session.New(session.Config{})
+var UserSessionStore = session.New(session.Config{})
 
-func Main(app *fiber.App) {
+var CsrfMiddleware = csrf.New(csrf.Config{
+	KeyLookup:      "header:X-Csrf-Token",
+	CookieName:     "csrf_",
+	CookieSameSite: "Lax",
+	Expiration:     1 * time.Hour,
+	KeyGenerator:   utils.UUIDv4,
+})
 
-	csrfMiddleware := csrf.New(csrf.Config{
-		KeyLookup:      "header:X-Csrf-Token",
-		CookieName:     "csrf_",
-		CookieSameSite: "Lax",
-		Expiration:     1 * time.Hour,
-		KeyGenerator:   utils.UUIDv4,
-	})
+func Setup(app *fiber.App) {
 
-	app.Post("/api/register", Register, csrfMiddleware)
-	app.Post("/api/login", Login, csrfMiddleware)
+	app.Post("/api/register", Register, CsrfMiddleware)
+	app.Post("/api/login", Login, CsrfMiddleware)
+	app.Post("/api/logout", Logout, CsrfMiddleware)
+
 	app.Get("/api/csrf", func(c fiber.Ctx) error {
 		return c.JSON(fiber.Map{"Valid": true})
-	}, csrfMiddleware)
+	}, CsrfMiddleware)
 
 }
