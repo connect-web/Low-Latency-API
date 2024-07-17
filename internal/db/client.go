cc792beb21a42f0b205632e5b1a482e2fbfb18b6
Elliot <141949359+webElliot@users.noreply.github.com>
2024-07-17 19:26:29 +0100

    added a GetURL method for authentication. This is used to store session cookies.

--- internal/db/client.go
+++ internal/db/client.go
@@ -23,6 +23,29 @@ func NewDBClient() *DBClient {
 	}
 }
 
+func GetUrl() string {
+	envVar := os.Getenv("siteonline")
+
+	var user, password, host, port, dbname string
+
+	if envVar == "site" {
+		user = os.Getenv("lowLatencyUser")
+		password = os.Getenv("lowLatencyPassword")
+		host = os.Getenv("lowLatencyHost")
+		port = os.Getenv("lowLatencyPort")
+		dbname = os.Getenv("lowLatencyDatabase")
+	} else {
+		// LOCALHOST connect to server credentials
+		user = os.Getenv("lowLatencyWebUser")
+		password = os.Getenv("lowLatencyWebPassword")
+		host = os.Getenv("lowLatencyWebHost")
+		port = os.Getenv("lowLatencyWebPort")
+		dbname = os.Getenv("lowLatencyWebDatabase")
+	}
+
+	return fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", user, password, host, port, dbname)
+}
+
 // Connect establishes a connection to the database.
 func (client *DBClient) Connect() error {
 
