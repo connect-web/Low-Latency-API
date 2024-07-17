d07587581353c154b75000f7a3c8b604918fe00c
Elliot <141949359+webElliot@users.noreply.github.com>
2024-07-17 16:00:17 +0100

    added sql

--- sql/tables.sql
+++ sql/tables.sql
@@ -3,10 +3,19 @@ CREATE SCHEMA IF NOT EXISTS users;
 CREATE TABLE IF NOT EXISTS users.accs
 (
     ID       SERIAL PRIMARY KEY,
+    TIME     timestamptz DEFAULT NOW(),
     name     CITEXT UNIQUE NOT NULL,
     password BYTEA         NOT NULL
 );
 
+
+
+CREATE TABLE IF NOT EXISTS users.fiber_storage
+(
+    k VARCHAR(64) PRIMARY KEY NOT NULL DEFAULT '',
+    v BYTEA                   NOT NULL,
+    e BIGINT                  NOT NULL DEFAULT '0'
+);
 
 CREATE SCHEMA IF NOT EXISTS PROFILES;
 -- don't create global stats yet, we don't have enough stats stored to make it worth it.
@@ -19,19 +28,22 @@ CREATE TABLE IF NOT EXISTS Profiles.global_stats
 
 CREATE TABLE IF NOT EXISTS Profiles.user_stats
 (
-    ID INTEGER PRIMARY KEY,
-    bots_tracked int,
-    bots_banned int,
-    bots_playerIds INT[],
-    banned_experience BIGINT,
-    players_added int, -- the number of users that were not in the system but the profile requested and were approved.
+    ID                INTEGER PRIMARY KEY,
+    TIME              timestamptz DEFAULT NOW(),
+    bots_tracked      int         DEFAULT 0,
+    bots_banned       int         DEFAULT 0,
+    bots_playerIds    INT[]       DEFAULT ARRAY []::integer[],
+    banned_experience BIGINT      default 0,
+    players_added     int         default 0, -- the number of users that were not in the system but the profile requested and were approved.
     FOREIGN KEY (ID) REFERENCES USERS.ACCS (id)
 );
 
 CREATE TABLE IF NOT EXISTS Profiles.user_player_requests
 (
-    ID INTEGER PRIMARY KEY,
-    REQUESTED_ACCOUNTS text[],
-    TIME timestamptz,
+    ID                 INTEGER PRIMARY KEY,
+    REQUESTED_ACCOUNTS text[]      DEFAULT ARRAY []::TEXT[],
+    TIME               timestamptz DEFAULT NOW(),
     FOREIGN KEY (ID) REFERENCES USERS.ACCS (id)
 );
+
+
