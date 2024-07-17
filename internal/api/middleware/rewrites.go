1e7d795f1b35189b0dec4f41fbf85ccae054c85f
Elliot <141949359+webElliot@users.noreply.github.com>
2024-07-17 19:23:34 +0100

    login and register are no longer templates so redirect rules no longer apply.

--- internal/api/middleware/rewrites.go
+++ internal/api/middleware/rewrites.go
@@ -16,22 +16,12 @@ func Rewrites(c fiber.Ctx) error {
 	avoid_containing_paths := []string{
 		"api",
 		"resources",
-	}
-
-	avoid_paths := []string{
-		"/login",
-		"/register",
+		"favicon",
 	}
 
 	fmt.Println(path)
 	for _, avoid_str := range avoid_containing_paths {
 		if strings.Contains(strings.ToLower(path), avoid_str) {
-			return c.Next()
-		}
-	}
-
-	for _, avoidable := range avoid_paths {
-		if strings.ToLower(path) == avoidable {
 			return c.Next()
 		}
 	}
