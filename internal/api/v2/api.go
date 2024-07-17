b7aa961d8020b0809cda4d0a97f38209fee8bbe0
Elliot <141949359+webElliot@users.noreply.github.com>
2024-07-17 19:25:19 +0100

    added public & user api's to V2

--- internal/api/v2/api.go
+++ internal/api/v2/api.go
@@ -1,20 +1,24 @@
 package v2
 
 import (
-	"github.com/connect-web/Low-Latency-API/internal/api/auth"
+	"github.com/connect-web/Low-Latency-API/internal/api/v2/global"
+	"github.com/connect-web/Low-Latency-API/internal/api/v2/profile"
 	"github.com/connect-web/Low-Latency-API/internal/api/v2/public"
 	"github.com/gofiber/fiber/v3"
 )
 
 func RegisterRouter(api fiber.Router) {
-	v2 := api.Group("/v2", auth.Protected)
-
-	publicRoute := v2.Group("/public")
+	publicRoute := api.Group("/public")
 
 	publicRoute.Get("/skill-toplist", public.GetSkillToplist)
 	publicRoute.Get("/skill-toplist-users", public.GetSkillToplistUsers)
 
 	publicRoute.Get("/boss-minigame-toplist", public.GetMinigameToplist)
 	publicRoute.Get("/boss-minigame-toplist-users", public.GetMinigameToplistUsers)
+
+	userRoute := api.Group("/user")
+
+	userRoute.Get("/profile", profile.GetProfile)
+	userRoute.Get("/ban-count", global.GetBanCount)
 
 }
