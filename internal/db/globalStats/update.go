eaa6c324be8f99c119d822a4daddcf562d131494
Elliot <141949359+webElliot@users.noreply.github.com>
2024-07-19 17:42:33 +0100

    added a routine task to update the global stats every 6 hours & save in memory the latest data.

--- internal/db/globalStats/update.go
+++ internal/db/globalStats/update.go
@@ -1,18 +1,15 @@
 package globalStats
 
 import (
-	"errors"
 	"fmt"
-	"github.com/connect-web/Low-Latency-API/internal/db"
 	"github.com/connect-web/Low-Latency-API/internal/model"
-	"log"
 	"time"
 )
 
 func GlobalStatsWorker() {
 	for {
 		UpdateGlobalStatistics()
-		time.Sleep(1 * time.Hour)
+		time.Sleep(1 * time.Minute)
 	}
 }
 
@@ -20,41 +17,17 @@ func UpdateGlobalStatistics() {
 	if !shouldUpdateGlobalStatistics() {
 		return
 	}
-	ban_count, err := queryTotalBans()
-	if err != nil {
-		return
+	global_stats, err := GetGlobalStats()
+	if err == nil {
+		model.LowLatencyStatistics = global_stats
 	}
-	model.Last_updated = time.Now()
-	model.TotalBans = ban_count
-}
-
-func queryTotalBans() (int, error) {
-	var total_bans int
-
-	query := "SELECT BANS FROM PROFILES.global_stats"
-	client := db.NewDBClient()
-	if connectErr := client.Connect(); connectErr != nil {
-		log.Println(connectErr.Error())
-		return total_bans, errors.New("Internal server error")
-	}
-	defer func() {
-		if closeConnErr := client.Close(); closeConnErr != nil {
-			log.Printf("Failed to close database connection: %v", closeConnErr)
-		}
-	}()
-
-	row := client.DB.QueryRow(query)
-
-	scanErr := row.Scan(&total_bans)
-
-	return total_bans, scanErr
 }
 
 func shouldUpdateGlobalStatistics() bool {
-	duration := time.Now().Sub(model.Last_updated)
+	duration := time.Now().Sub(model.LowLatencyStatistics.Last_updated)
 	hoursSinceLastUpdate := duration.Hours()
 	fmt.Printf("Hours since last update: %.2f\n", hoursSinceLastUpdate)
 
-	return 12 < hoursSinceLastUpdate
+	return 6 < hoursSinceLastUpdate
 	// pass
 }
