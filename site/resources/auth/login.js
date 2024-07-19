b562f54a3cc6d11e78a780b69b2f39fd9a160a0f
Elliot <141949359+webElliot@users.noreply.github.com>
2024-07-19 17:37:07 +0100

    login api has error handling, loading display, msg boxes & CSRF revalidation.

--- site/resources/auth/login.js
+++ site/resources/auth/login.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
     const form = document.getElementById("loginForm");
+    const errorDialog = document.getElementById("Error");
+
 
     form.addEventListener("submit", function(event) {
         event.preventDefault();
-
+        ShowLoading();
         const csrfToken = fetchCsrfTokenIfNeeded();
 
         const formData = new FormData(form);
@@ -13,17 +15,67 @@ document.addEventListener("DOMContentLoaded", function() {
             jsonData[key] = value;
         });
 
+
         sendRequest('/api/auth/login', jsonData, csrfToken)
             .then(data => {
                 console.log(data);
-                if (data.message === "Logged in"){
+                if (data.message === "Logged in") {
                     window.location.replace("/profile");
+                }else{
+                    handleError(data.error)
                 }
                 // Handle the response data
             })
             .catch(error => {
-                console.error("Error:", error);
-                // Handle the error
+                if (error.message === 'CSRF token is invalid or missing') {
+                    // Retry the request with a new CSRF token
+                    const newCsrfToken = fetchCsrfTokenIfNeeded();
+                    sendRequest('/api/auth/login', jsonData, newCsrfToken)
+                        .then(data => {
+                            console.log(data);
+                            HideLoading();
+                            if (data.message === "Logged in") {
+                                window.location.replace("/profile");
+                            }
+
+                            // Handle the response data
+                        })
+                        .catch(err => {
+                            console.error("Retry error:", err);
+                            HideLoading();
+
+                            // Handle the retry error
+                        });
+                } else {
+                    console.error("Error:", error);
+                    HideLoading();
+                    // Handle the error
+                }
             });
+
+    });
+
+    // Add click event listener to the error dialog to hide it when clicked
+    errorDialog.addEventListener("click", function() {
+        errorDialog.classList.add("hidden");
     });
 });
+
+
+
+function handleError(err) {
+    HideLoading();
+    console.error("Handling err" +err)
+    if (err.includes("Captcha failed")) {
+        reloadCaptcha();
+    }
+    const errorDialog = document.getElementById("Error");
+    const errorMessage = document.getElementById("errorMessage");
+    errorMessage.textContent = err;
+    errorDialog.classList.remove("hidden");
+}
+
+// Function to reload the CAPTCHA
+function reloadCaptcha() {
+    grecaptcha.reset();
+}
