9f573efb9e3fb5a7aa035d289d2c1dcc254582d6
Elliot <141949359+webElliot@users.noreply.github.com>
2024-07-19 17:41:20 +0100

    added the new ML routes to API

--- internal/api/v2/api.go
+++ internal/api/v2/api.go
@@ -2,6 +2,7 @@ package v2
 
 import (
 	"github.com/connect-web/Low-Latency-API/internal/api/v2/global"
+	"github.com/connect-web/Low-Latency-API/internal/api/v2/ml"
 	"github.com/connect-web/Low-Latency-API/internal/api/v2/profile"
 	"github.com/connect-web/Low-Latency-API/internal/api/v2/public"
 	"github.com/gofiber/fiber/v3"
@@ -16,9 +17,16 @@ func RegisterRouter(api fiber.Router) {
 	publicRoute.Get("/boss-minigame-toplist", public.GetMinigameToplist)
 	publicRoute.Get("/boss-minigame-toplist-users", public.GetMinigameToplistUsers)
 
+	MlRoute := api.Group("/ml")
+	MlRoute.Get("/skill-toplist", ml.GetSkillToplist)
+	MlRoute.Get("/skill-toplist-users", ml.GetSkillToplistUsers)
+
+	MlRoute.Get("/boss-minigame-toplist", ml.GetMinigameToplist)
+	MlRoute.Get("/boss-minigame-toplist-users", ml.GetMinigameToplistUsers)
+
 	userRoute := api.Group("/user")
 
 	userRoute.Get("/profile", profile.GetProfile)
-	userRoute.Get("/ban-count", global.GetBanCount)
+	userRoute.Get("/global-stats", global.GetLowLatencyStats)
 
 }
