d7b088d83f7145950736f0d8dcb908c48ca98d4c
Elliot <141949359+webElliot@users.noreply.github.com>
2024-07-19 17:41:06 +0100

    added ML backend queries & data fetchers for /ml API's

---
+++ internal/api/v2/ml/handlers.go
@@ -0,0 +1,78 @@
+package ml
+
+import (
+	"fmt"
+	"github.com/connect-web/Low-Latency-API/internal/util"
+	"github.com/gofiber/fiber/v3"
+)
+
+/* *** SKILLER TOPLISTS *** */
+
+func GetSkillToplist(c fiber.Ctx) error {
+	skillers, err := QuerySkillToplist()
+	if err != nil {
+		return util.InternalServerError(c)
+	}
+
+	if len(skillers) == 0 {
+		return util.NoPlayersFound(c)
+	}
+
+	return c.JSON(skillers)
+}
+
+func GetSkillToplistUsers(c fiber.Ctx) error {
+	string_skill := c.Query("skill")
+	valid := util.ValidSkill(string_skill)
+	if !valid {
+		return util.InternalServerError(c)
+	}
+	players, err := QuerySkillToplistUsers(string_skill)
+	if err != nil {
+		fmt.Println(err.Error())
+		return util.InternalServerError(c)
+	}
+
+	if len(players) == 0 {
+		return util.NoPlayersFound(c)
+	}
+
+	return c.JSON(players)
+}
+
+/*
+*** Boss & Minigame Toplists ***
+ */
+
+func GetMinigameToplist(c fiber.Ctx) error {
+	minigamers, err := QueryMinigameToplist()
+	if err != nil {
+		return util.InternalServerError(c)
+	}
+
+	if len(minigamers) == 0 {
+		return util.NoPlayersFound(c)
+	}
+
+	return c.JSON(minigamers)
+}
+
+func GetMinigameToplistUsers(c fiber.Ctx) error {
+	string_skill := c.Query("minigame") // this should be converted to an ID
+
+	valid := util.ValidMinigame(string_skill)
+	if !valid {
+		return util.InternalServerError(c)
+	}
+
+	players, err := QueryMinigameToplistUsers(string_skill)
+	if err != nil {
+		return util.InternalServerError(c)
+	}
+
+	if len(players) == 0 {
+		return util.NoPlayersFound(c)
+	}
+
+	return c.JSON(players)
+}
---
+++ internal/api/v2/ml/minigame_queries.go
@@ -0,0 +1,93 @@
+package ml
+
+import (
+	"errors"
+	"github.com/connect-web/Low-Latency-API/internal/db"
+	"github.com/connect-web/Low-Latency-API/internal/db/Scanner"
+	"github.com/connect-web/Low-Latency-API/internal/model"
+	"log"
+)
+
+var MINIGAME_TOPLIST_QUERY = `
+SELECT activity, count(distinct playerid)
+FROM ML.results
+WHERE activitytype = 'minigames'
+GROUP BY activity
+ORDER BY count(distinct playerid) desc;
+`
+
+var MINIGAME_TOPLIST_USER_QUERY = `
+	SELECT p.name,
+       pls.combat_level,
+       pls.overall,
+       pls.total_level,
+       pl.skills_experience,
+       pl.skills_ratio,
+       pl.skills_levels,
+       pl.minigames,
+       pg.skills_experience,
+       pg.skills_ratio,
+       pg.minigames
+FROM ML.results links
+         LEFT JOIN PLAYERS P on p.id = links.playerid
+         LEFT JOIN player_live pl on pl.playerid = links.playerid
+         LEFT JOIN player_live_stats pls on pls.playerid = links.playerid
+         LEFT JOIN player_gains pg on pg.playerid = links.playerid
+WHERE links.activity = $1
+AND links.activityType = 'minigames'
+ORDER BY links DESC;
+	`
+
+func QueryMinigameToplist() ([]model.MinigameToplist, error) {
+	results := []model.MinigameToplist{}
+	client := db.NewDBClient()
+	if connectErr := client.Connect(); connectErr != nil {
+		log.Println(connectErr.Error())
+		return results, errors.New("Internal server error")
+	}
+	defer func() {
+		if closeConnErr := client.Close(); closeConnErr != nil {
+			log.Printf("Failed to close database connection: %v", closeConnErr)
+		}
+	}()
+
+	rows, queryErr := client.DB.Query(MINIGAME_TOPLIST_QUERY)
+	if queryErr != nil {
+		log.Println("Error executing query:", queryErr.Error())
+		return results, errors.New("Internal server error")
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		row := model.MinigameToplist{}
+		if err := rows.Scan(&row.Minigame, &row.Count); err == nil {
+			results = append(results, row)
+		}
+	}
+	return results, nil
+}
+
+func QueryMinigameToplistUsers(minigame string) ([]model.Player, error) {
+	results := []model.Player{}
+
+	client := db.NewDBClient()
+	if connectErr := client.Connect(); connectErr != nil {
+		log.Println(connectErr.Error())
+		return results, errors.New("Internal server error")
+	}
+	defer func() {
+		if closeConnErr := client.Close(); closeConnErr != nil {
+			log.Printf("Failed to close database connection: %v", closeConnErr)
+		}
+	}()
+
+	rows, queryErr := client.DB.Query(MINIGAME_TOPLIST_USER_QUERY, minigame)
+	if queryErr != nil {
+		log.Println("Error executing query:", queryErr.Error())
+		return results, errors.New("Internal server error")
+	}
+	defer rows.Close()
+
+	results, err := Scanner.ScanPlayerRows(rows)
+	return results, err
+}
