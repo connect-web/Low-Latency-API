a7dcf21478de17f8945529cd957c07b73a02b614
Elliot <141949359+webElliot@users.noreply.github.com>
2024-07-23 00:14:16 +0100

    ml api now returns model metrics

--- internal/model/Toplist.go
+++ internal/model/Toplist.go
@@ -8,6 +8,16 @@ type SkillToplist struct {
 }
 
 type MinigameToplist struct {
-	Minigame string `json:"minigame"`
-	Count    int    `json:"count"`
+	Minigame string                 `json:"minigame"`
+	Count    int                    `json:"count"`
+	Metrics  MachineLearningMetrics `json:"Metrics"`
+}
+
+type MachineLearningMetrics struct {
+	ROCAUC         float64 `json:"ROC-AUC"`
+	MeanAccuracy   float64 `json:"Mean Accuracy"`
+	RecallClass0   float64 `json:"Recall Class 0"`
+	RecallClass1   float64 `json:"Recall Class 1"`
+	AccuracyClass0 float64 `json:"Accuracy Class 0"`
+	AccuracyClass1 float64 `json:"Accuracy Class 1"`
 }
