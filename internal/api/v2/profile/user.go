37c25b80f4297a1cd5c8fa800f7db3588023e43d
Elliot <141949359+webElliot@users.noreply.github.com>
2024-08-05 19:53:22 +0100

    removed redundant code for faster response to api
    - This will have variables soon.

--- internal/api/v2/profile/user.go
+++ internal/api/v2/profile/user.go
@@ -2,23 +2,28 @@ package profile
 
 import (
 	"github.com/connect-web/Low-Latency-API/internal/api/auth"
-	"github.com/connect-web/Low-Latency-API/internal/db/user"
+	"github.com/connect-web/Low-Latency-API/internal/model"
 	"github.com/connect-web/Low-Latency-API/internal/util"
 	"github.com/gofiber/fiber/v3"
 )
 
 func GetProfile(c fiber.Ctx) error {
+	// method does not serve any purpose currently.
+
 	username, err := auth.GetUsername(c)
 	if err != nil || username == "" {
 		return util.InternalServerError(c)
 	}
 
-	profile, profileFetchErr := user.FetchOrCreateProfile(username)
-	if profileFetchErr != nil {
-		return util.InternalServerError(c)
-	}
+	/*
+		profile, profileFetchErr := user.FetchOrCreateProfile(username)
+		if profileFetchErr != nil {
+			return util.InternalServerError(c)
+		}
 
-	profile.Username = username
+		profile.Username = username
 
-	return c.JSON(profile)
+	*/
+
+	return c.JSON(model.ProfileStats{Username: username})
 }
