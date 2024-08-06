396be9419b683d87f2ae0c71d0bd72f39ce65d74
Elliot <141949359+webElliot@users.noreply.github.com>
2024-08-06 13:33:38 +0100

    moved the auth session store creation into auth package.

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -1,21 +1,16 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"github.com/connect-web/Low-Latency-API/internal/api"
 	"github.com/connect-web/Low-Latency-API/internal/api/auth"
 	"github.com/connect-web/Low-Latency-API/internal/api/middleware"
 	"github.com/connect-web/Low-Latency-API/internal/api/templates"
-	"github.com/connect-web/Low-Latency-API/internal/db"
 	"github.com/connect-web/Low-Latency-API/internal/db/globalStats"
 	cache "github.com/connect-web/low-latency-cache-controller/wrapper"
-	"github.com/connect-web/storageself/postgres"
 	json "github.com/goccy/go-json"
 	"github.com/gofiber/fiber/v3"
-	"github.com/gofiber/fiber/v3/middleware/session"
 	"github.com/gofiber/template/html/v2"
-	"github.com/jackc/pgx/v5/pgxpool"
 	"log"
 	"os"
 	"time"
@@ -39,29 +34,6 @@ func listRoutes(app *fiber.App) {
 func main() {
 	go globalstats.GlobalStatsWorker() // starts a worker that periodically updates the global ban count statistics
 
-	config, err := pgxpool.ParseConfig(db.GetUrl())
-
-	if err != nil {
-		log.Fatalf("Unable to parse DATABASE_URL %v\n", err)
-	}
-
-	config.MaxConns = 2
-
-	pool, connectConfigErr := pgxpool.NewWithConfig(context.Background(), config)
-	if connectConfigErr != nil {
-		log.Fatalf("Unable to create connection pool: %v\n", err)
-	}
-	defer pool.Close()
-
-	store := postgres.New(
-		postgres.Config{
-			ConnectionURI: db.GetUrl(),
-			Table:         "users.fiber_storage",
-			Reset:         false,
-			GCInterval:    1 * time.Minute,
-		})
-	auth.UserSessionStore = session.New(session.Config{Storage: store})
-
 	engine := html.New("../../templates", ".html")
 
 	// Create a new Fiber app with the HTML engine
@@ -70,7 +42,7 @@ func main() {
 		JSONEncoder: json.Marshal,
 		JSONDecoder: json.Unmarshal,
 	})
-
+	auth.CreateSessionStore()
 	// Setup middleware for all Routers
 	middleware.Run(app)
 	// templates.Run(app) // templates not used yet
