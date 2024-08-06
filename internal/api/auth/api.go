b3e7781cd3efe1c36b2897547216fdcc60eb0b1d
Elliot <141949359+webElliot@users.noreply.github.com>
2024-08-06 13:33:42 +0100

    moved the auth session store creation into auth package.

--- internal/api/auth/api.go
+++ internal/api/auth/api.go
@@ -1,14 +1,44 @@
 package auth
 
 import (
+	"github.com/connect-web/Low-Latency-API/internal/db"
+	"github.com/connect-web/storageself/postgres"
 	"github.com/gofiber/fiber/v3"
 	"github.com/gofiber/fiber/v3/middleware/csrf"
 	"github.com/gofiber/fiber/v3/middleware/session"
 	"github.com/gofiber/utils/v2"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"golang.org/x/net/context"
+	"log"
 	"time"
 )
 
-var UserSessionStore = session.New(session.Config{})
+var UserSessionStore *session.Store
+
+func CreateSessionStore() {
+	config, err := pgxpool.ParseConfig(db.GetUrl())
+
+	if err != nil {
+		log.Fatalf("Unable to parse DATABASE_URL %v\n", err)
+	}
+
+	config.MaxConns = 2
+
+	pool, connectConfigErr := pgxpool.NewWithConfig(context.Background(), config)
+	if connectConfigErr != nil {
+		log.Fatalf("Unable to create connection pool: %v\n", err)
+	}
+	defer pool.Close()
+
+	store := postgres.New(
+		postgres.Config{
+			ConnectionURI: db.GetUrl(),
+			Table:         "users.fiber_storage",
+			Reset:         false,
+			GCInterval:    1 * time.Minute,
+		})
+	UserSessionStore = session.New(session.Config{Storage: store})
+}
 
 var CsrfMiddleware = csrf.New(csrf.Config{
 	KeyLookup:      "header:X-Csrf-Token",
